698bae9118a5b58d6adadfc4ae6b1796e21f72d8
Tony Lea <tnylea@gmail.com>
2024-04-26 11:00:32 -0400

    Adding update to DevDojo Auth Page

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,23 @@
 module.exports = {
     content: ["./**/*.{html,js}"],
     theme: {
-        extend: {},
+        extend: {
+            animation: {
+                      marquee: 'marquee 25s linear infinite',
+                      'marquee-reverse': 'marquee 25s linear infinite reverse',
+                      'marquee-horizontal': 'marqueeHorizontal 40s linear infinite',
+                  },
+            keyframes: {
+                      marquee: {
+                          from: { transform: 'translateY(0)' },
+                          to: { transform: 'translateY(calc(-50% - var(--gap)/2))' },
+                      },
+                      marqueeHorizontal: {
+                          from: { transform: 'translateX(0)' },
+                          to: { transform: 'translateX(calc(-100% - var(--gap)))' },
+                      },
+                  },
+        },
     },
     plugins: [
         require('@tailwindcss/typography'),
