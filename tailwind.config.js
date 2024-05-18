1b3e4ab8a5b1cac76853fd15fe2474dc9838e660
Tony Lea <tnylea@gmail.com>
2024-05-18 09:37:40 -0400

    updating the tailwind config to work with the updated md styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./**/*.{html,js}"],
+    content: ["./**/*.{html,js,md}"],
     theme: {
         extend: {
             animation: {
